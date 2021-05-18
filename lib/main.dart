3dd3fe95540cbe2dd42a5f8f4179715da5489f10
Jikmat Choskit <jikmat303@gmail.com>
2021-05-18 14:53:52 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/shop.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'My First Flutter App',
+    home: Shop(),
+  ));
+}
